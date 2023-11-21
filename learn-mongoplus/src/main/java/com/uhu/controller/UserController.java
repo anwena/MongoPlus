073c319d53d839dc6035342b5ef66b40734b03c3
bomber <3107005787@qq.com>
2023-11-21 15:57:44 +0800

    添加solon的app

--- learn-mongoplus/src/main/java/com/uhu/controller/UserController.java
+++ learn-mongoplus/src/main/java/com/uhu/controller/UserController.java
@@ -2,6 +2,9 @@ package com.uhu.controller;
 
 import com.uhu.entity.User;
 import com.uhu.mapper.UserMapper;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Get;
+import org.noear.solon.annotation.Mapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,26 +19,35 @@ import java.util.List;
  * @CreateTime: 2023/11/16 10:09
  */
 @RestController
+@Controller
 public class UserController {
 
     @Resource
     private UserMapper userMapper;
 
+    @Get
+    @Mapping("/user/{id}")
     @GetMapping("/user/{id}")
     public User getById(@PathVariable Integer id) {
         return userMapper.getById(id);
     }
 
+    @Get
+    @Mapping("/one/{id}")
     @GetMapping("/one/{id}")
     public User test1(@PathVariable Integer id) {
         return userMapper.getOneById(id);
     }
 
+    @Get
+    @Mapping("/list/{id}")
     @GetMapping("/list/{id}")
     public List<User> test2(@PathVariable Integer id) {
         return userMapper.getList(id);
     }
 
+    @Get
+    @Mapping("/array/{id}")
     @GetMapping("/array/{id}")
     public User[] test3(@PathVariable Integer id) {
         return userMapper.getArray(id);
